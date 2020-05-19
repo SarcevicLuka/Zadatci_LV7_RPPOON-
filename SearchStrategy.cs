0cba556a01b0db4d7283b79c0baa0acd5f315253
Luka Šarčević <5227796-luka_sarcevic@users.noreply.gitlab.com>
2020-05-19 17:12:06 +0000

    Add new file

---
+++ SearchStrategy.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prvi_i_drugi_zadatak
+{
+    abstract class SearchStrategy
+    {
+        public abstract double Search(double[] array, double numberToSearch);
+    }
+}
