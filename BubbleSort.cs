e3f41bf4f3aec9b769d30d519f0c2e10e00c8738
Luka Šarčević <5227796-luka_sarcevic@users.noreply.gitlab.com>
2020-05-19 17:14:31 +0000

    Add new file

---
+++ BubbleSort.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prvi_i_drugi_zadatak
+{
+    class BubbleSort : SortStrategy
+    {
+        public override void Sort(double[] array)
+        {
+            int arraySize = array.Length;
+            for(int i = 0; i < arraySize - 1; i++)
+            {
+                for(int j = 0; j < arraySize - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                        Swap(ref array[j], ref array[j + 1]);
+                }
+            }
+        }
+    }
+}
