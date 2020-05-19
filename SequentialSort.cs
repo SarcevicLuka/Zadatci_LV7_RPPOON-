f5139db7f7fdb272149491509436167ff5281674
Luka Šarčević <5227796-luka_sarcevic@users.noreply.gitlab.com>
2020-05-19 17:11:42 +0000

    Add new file

---
+++ SequentialSort.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prvi_i_drugi_zadatak
+{
+    class SequentialSort : SortStrategy
+    {
+        public override void Sort(double[] array)
+        {
+            int arraySize = array.Length;
+            for (int i = 0; i < arraySize - 1; i++)
+            {
+                for (int j = i + 1; j < arraySize; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+        }
+    }
+}
