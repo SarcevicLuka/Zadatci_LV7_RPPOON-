08848b2e83cd52efdb214ca3b0e070121e0e6dac
Luka Šarčević <5227796-luka_sarcevic@users.noreply.gitlab.com>
2020-05-19 17:09:31 +0000

    Add new file

---
+++ Logger.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treći_i_četvrti_zadatak
+{
+    interface Logger
+    {
+        void Log(SimpleSystemDataProvider provider);
+    }
+}
